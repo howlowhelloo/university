015ceb67361784913e287bb80bb75478c3c7b816
howlowhelloo <149948612+howlowhelloo@users.noreply.github.com>
2025-05-29 03:45:19 +0300

    Update urls.py

--- university/urls.py
+++ university/urls.py
@@ -18,12 +18,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('students.urls')),   # students.urls должен содержать путь add/
     path('app2/', include('app2.urls')),
-    path('', admin.site.urls)
+    path('', RedirectView.as_view(url='add/', permanent=False))
 ]
 
 if settings.DEBUG:
